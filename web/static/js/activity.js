4442099816d8be8fe9ab200a9da9a2ed83243568
Antoine <antoine@cercle.co>
2017-03-12 23:49:58 +0100

    code hygiene improve clarity of JS

--- web/static/js/activity.js
+++ web/static/js/activity.js
@@ -1,26 +1,26 @@
 export var Activity = {
   start: function(){
-    var jwt_token = document.querySelector('meta[name="guardian_token"]').content;
+    var jwtToken = document.querySelector('meta[name="guardian_token"]').content;
     $('.activity_page_toggle').iCheck({
-         checkboxClass: 'icheckbox_square-blue',
-         radioClass: 'iradio_square-blue',
-         increaseArea: '20%' // optional
-      });
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass: 'iradio_square-blue',
+      increaseArea: '20%' // optional
+    });
   
     $('.activity_page_toggle').on('ifToggled', function(event){
-        var url = '/api/v2/activity/' + $(this).data('id');
-        var that = $(this);
-        $.ajax( url , {
-            method: 'PUT',
-            headers: {"Authorization": "Bearer "+jwt_token},
-            data: { 'activity[is_done]': $(this).is(":checked") },
-            complete: function(xhr, status){
-              if(that.is(":checked")){
-                $('#row_task_'+that.data('id')).remove();
-              }
-              return true;
-            }
-        });
+      var url = '/api/v2/activity/' + $(this).data('id');
+      var that = $(this);
+      $.ajax( url , {
+        method: 'PUT',
+        headers: {'Authorization': 'Bearer '+jwtToken},
+        data: { 'activity[is_done]': $(this).is(':checked') },
+        complete: function(xhr, status){
+          if(that.is(':checked')){
+            $('#row_task_'+that.data('id')).remove();
+          }
+          return true;
+        }
+      });
     });
   }
-}
+};
