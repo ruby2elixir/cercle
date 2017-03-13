2bb683c3671dd08fa7227a7ab7012899892f8a47
Maxim Pechnikov <parallel588@gmail.com>
2017-03-13 09:59:23 +0300

    " -> '

--- brunch-config.js
+++ brunch-config.js
@@ -12,31 +12,31 @@ exports.config = {
             // To use a separate vendor.js bundle, specify two files path
             // https://github.com/brunch/brunch/blob/stable/docs/config.md#files
             // joinTo: {
-            //  "js/app.js": /^(web\/static\/js)/,
-            //  "js/vendor.js": /^(web\/static\/vendor)|(deps)/
+            //  'js/app.js': /^(web\/static\/js)/,
+            //  'js/vendor.js': /^(web\/static\/vendor)|(deps)/
             // }
             //
             // To change the order of concatenation of files, explicitly mention here
             // https://github.com/brunch/brunch/tree/master/docs#concatenation
             // order: {
             //   before: [
-            //     "web/static/vendor/js/jquery-2.1.1.js",
-            //     "web/static/vendor/js/bootstrap.min.js"
+            //     'web/static/vendor/js/jquery-2.1.1.js',
+            //     'web/static/vendor/js/bootstrap.min.js'
             //   ]
             // }
         },
         stylesheets: {
-            joinTo: "css/app.css"
+            joinTo: 'css/app.css'
         },
         templates: {
-            joinTo: "js/app.js"
+            joinTo: 'js/app.js'
         }
     },
 
     conventions: {
         // This option sets where we should place non-css and non-js assets in.
-        // By default, we set this to "/web/static/assets". Files in this directory
-        // will be copied to `paths.public`, which is "priv/static" by default.
+        // By default, we set this to '/web/static/assets'. Files in this directory
+        // will be copied to `paths.public`, which is 'priv/static' by default.
         assets: /^(web\/static\/assets)/
     },
 
@@ -44,12 +44,12 @@ exports.config = {
     paths: {
         // Dependencies and current project directories to watch
         watched: [
-            "web/static",
-            "test/static"
+            'web/static',
+            'test/static'
         ],
 
         // Where to compile files to
-        public: "priv/static"
+        public: 'priv/static'
     },
 
     // Configure your plugins
@@ -66,7 +66,7 @@ exports.config = {
 
     modules: {
         autoRequire: {
-            "js/app.js": ["web/static/js/app"]
+            'js/app.js': ['web/static/js/app']
         }
     },
 
@@ -83,6 +83,6 @@ exports.config = {
         },
         // Whitelist the npm deps to be pulled in as front-end assets.
         // All other deps in package.json will be excluded from the bundle.
-        whitelist: ["phoenix", "phoenix_html", "jquery", "bootstrap-datepicker", "vue-select", 'vue-resource', 'vue-strap', 'moment']
+        whitelist: ['phoenix', 'phoenix_html', 'jquery', 'bootstrap-datepicker', 'vue-select', 'vue-resource', 'vue-strap', 'moment']
     }
 };
