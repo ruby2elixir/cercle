1513919ff8d209aec620b6e2f61a928a6bee6516
Prateek Gupta <prateek@akaruilabs.com>
2017-03-28 23:26:36 +0530

    fix indentation

--- lib/cercleApi/helpers/handle_unauthorized.ex
+++ lib/cercleApi/helpers/handle_unauthorized.ex
@@ -2,33 +2,33 @@ defmodule CercleApi.Helpers do
   import Plug.Conn
   import CercleApi.Router.Helpers
 
-	def handle_unauthorized(conn) do
-		conn
-		|> Phoenix.Controller.put_flash(:error, "You are not authorized for this action!")
-		|> Phoenix.Controller.redirect(to: "/")
-		|> halt
-	end
-
-	def handle_not_found(conn) do
-		conn
-		|> Phoenix.Controller.put_flash(:error, "Resource not found")
-		|> Phoenix.Controller.redirect(to: "/")
-		|> halt
+  def handle_unauthorized(conn) do
+    conn
+    |> Phoenix.Controller.put_flash(:error, "You are not authorized for this action!")
+    |> Phoenix.Controller.redirect(to: "/")
+    |> halt
   end
 
-	def handle_json_unauthorized(conn) do
-		conn
-		|> respond(403, "You are not authorized for this action!")
-		|> halt
+  def handle_not_found(conn) do
+    conn
+    |> Phoenix.Controller.put_flash(:error, "Resource not found")
+    |> Phoenix.Controller.redirect(to: "/")
+    |> halt
   end
+
+  def handle_json_unauthorized(conn) do
+    conn
+    |> respond(403, "You are not authorized for this action!")
+    |> halt
+    end
 
   def handle_json_not_found(conn) do
-		conn
-		|> respond(404, "Resource not found")
-		|> halt
+    conn
+    |> respond(404, "Resource not found")
+    |> halt
   end
 
-	defp respond(conn, status, msg) do
+  defp respond(conn, status, msg) do
     try do
       conn
       |> put_resp_content_type("application/json")
