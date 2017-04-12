b5043847c7afda284bd19ed3dda9bc0c0ecc53e7
Maxim Pechnikov <parallel588@gmail.com>
2017-04-12 13:06:06 +0300

    comments unused test

--- web/channels/timeline_event_channel.ex
+++ web/channels/timeline_event_channel.ex
@@ -3,13 +3,8 @@ defmodule CercleApi.TimelineEventChannel do
   require Logger
   alias CercleApi.{Contact, Repo}
 
-  def join("timeline_event:" <> contact_id, payload, socket) do
+  def join("timeline_event:" <> contact_id, _payload, socket) do
     socket = assign(socket, :contact_id, contact_id)
     {:ok, socket}
   end
-
-  # def handle_in("event:create", payload, socket) do
-
-  #   {:reply, {:ok, payload}, socket}
-  # end
 end
