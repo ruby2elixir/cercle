9d4b789d89be2011be87310b49848bc57503825d
Maxim Pechnikov <parallel588@gmail.com>
2017-04-18 15:53:16 +0300

    fix load activity page

---
+++ web/static/components/activities/item.vue
@@ -0,0 +1,47 @@
+<template>
+ <tr>
+  <td style="width:30px;">
+    <img :src="'/images/letters/' + item.contact.name.slice(0,1).toLowerCase() + '.png'" style="border-radius:14px;" />
+  </td>
+  <td style="width:150px;">
+    <a v-on:click.stop="$emit('contact-show')" href='#' style="color: #565656;"> {{item.contact && item.contact.name}} </a>
+  </td>
+  <td style="font-size:16px;">
+  <a style="color: #565656;"> <span style=""> {{item.title}}</span> </a><br />
+  </td>
+  <td style="width:150px;">
+    {{item.due_date| moment(timeFormat) }}
+  </td>
+  <td style="width:30px;">
+  <el-checkbox v-model="item.is_done" v-on:change="updateTask(item)"></el-checkbox>
+  </td>
+</tr>
+</template>
+
+<script>
+    export default {
+      props: {
+        'timeFormat': { type: String, default: 'ddd DD MMM @ h:m' },
+        item: {
+          type: Object,
+          default: function() { return {}; }
+        }
+      },
+      methods: {
+        updateTask(task) {
+          let url = '/api/v2/activity/' + task.id;
+
+          this.$http.put(url, {
+            activity: { isDone: task.is_done }
+          });
+
+        }
+      },
+      components: {
+        'el-checkbox': Element.Checkbox
+      }
+};
+</script>
+<style lang="sass">
+
+</style>
--- web/static/components/activities/list.vue
+++ web/static/components/activities/list.vue
@@ -2,20 +2,26 @@
   import {Socket, Presence} from 'phoenix';
   import InlineEdit from '../inline-common-edit.vue';
   import ContactForm from '../contacts/edit.vue';
+  import ActivityItem from './item.vue';
 
   export default {
     data() {
       return {
+        timeZone: null,
         showContact: false,
         contact: {},
         opportunity: {},
-        contactView: null
+        contactView: null,
+        activitiesOverdue: [],
+        activitiesLater: [],
+        activitiesToday: []
       };
     },
     components: {
       'inline-edit': InlineEdit,
       'modal': VueStrap.modal,
-      'contact-form': ContactForm
+      'contact-form': ContactForm,
+      'activity-item': ActivityItem
     },
     methods: {
       contactShow(contactId, opportunityId) {
@@ -24,9 +30,23 @@
         this.opportunity = { id: opportunityId };
         this.contactView = 'contact-form';
         this.showContact = true;
-      }
-    },
+      },
+      initConn() {
+        this.$http.get('/api/v2/activity').then(resp => {
+          this.activitiesOverdue = resp.data.activities.overdue;
+          this.activitiesToday = resp.data.activities.today;
+          this.activitiesLater = resp.data.activities.later;
+        });
+      },
+      setAuthToken(){
+        var vm = this;
+        localStorage.setItem('auth_token', document.querySelector('meta[name="guardian_token"]').content);
+        Vue.http.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('auth_token');
+        vm.initConn();
+      }},
     mounted() {
+      this.timeZone = document.querySelector('meta[name="time_zone"]').content;
+      this.setAuthToken();
     }
   };
 </script>
