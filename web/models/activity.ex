9ad0d115fe7df401b4bd4b933019c2cb833f476a
Maxim Pechnikov <parallel588@gmail.com>
2017-04-19 18:36:45 +0300

    refactoring controller/api/activity

--- web/models/activity.ex
+++ web/models/activity.ex
@@ -1,5 +1,6 @@
 defmodule CercleApi.Activity do
   use CercleApi.Web, :model
+  use Timex
 
   @derive {Poison.Encoder, only: [
               :id, :title, :is_done, :due_date,
@@ -30,5 +31,55 @@ defmodule CercleApi.Activity do
           :user_id, :contact_id, :company_id, :due_date, :is_done,
           :title, :opportunity_id])
     |> validate_required([:user_id, :contact_id, :company_id])
+  end
+
+  def today(user) do
+    from_time = user.time_zone |> Timex.now |> Timex.beginning_of_day
+    to_time = user.time_zone |> Timex.now |> Timex.end_of_day
+
+    query = from p in CercleApi.Activity,
+      where: p.is_done == false,
+      where: p.user_id == ^user.id,
+      where: p.company_id == ^user.company_id,
+      where: p.due_date  >= ^from_time,
+      where: p.due_date <= ^to_time,
+      order_by: [asc: p.due_date]
+
+    query
+    |> CercleApi.Repo.all
+    |> CercleApi.Repo.preload([:contact, :user])
+  end
+
+  def overdue(user) do
+    from_time = user.time_zone |> Timex.now |> Timex.beginning_of_day
+    to_time = user.time_zone |> Timex.now |> Timex.end_of_day
+
+    query = from p in CercleApi.Activity,
+      where: p.is_done == false,
+      where: p.user_id == ^user.id,
+      where: p.company_id == ^user.company_id,
+      where: p.due_date  <= ^from_time,
+      where: not is_nil(p.contact_id),
+      order_by: [asc: p.due_date]
+
+    query
+    |> CercleApi.Repo.all
+    |> CercleApi.Repo.preload([:contact, :user])
+  end
+
+  def later(user) do
+    from_time = user.time_zone |> Timex.now |> Timex.beginning_of_day
+    to_time = user.time_zone |> Timex.now |> Timex.end_of_day
+
+    query = from p in CercleApi.Activity,
+      where: p.is_done == false,
+      where: p.user_id == ^user.id,
+      where: p.company_id == ^user.company_id,
+      where: p.due_date >= ^to_time,
+      order_by: [asc: p.due_date]
+
+    query
+    |> CercleApi.Repo.all
+    |> CercleApi.Repo.preload([:contact, :user])
   end
 end
