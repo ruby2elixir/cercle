93725cb41c589e711bf78046851d2520fb61b783
Harish Premkumar <p.harish86@gmail.com>
2017-04-22 08:44:43 +0000

    Option to create new card on board page

--- web/static/components/boards/board.vue
+++ web/static/components/boards/board.vue
@@ -2,6 +2,7 @@
   import {Socket, Presence} from 'phoenix';
   import InlineEdit from '../inline-common-edit.vue';
   import ContactForm from '../contacts/edit.vue';
+  import NewCard from './new-card.vue';
 
   export default {
     props: ['board_id'],
@@ -17,7 +18,8 @@
     components: {
       'inline-edit': InlineEdit,
       'modal': VueStrap.modal,
-      'contact-form': ContactForm
+      'contact-form': ContactForm,
+      'new-card': NewCard
     },
     methods: {
       contactShow(contactId, opportunityId) {
@@ -26,6 +28,53 @@
         this.opportunity = { id: opportunityId };
         this.contactView = 'contact-form';
         this.showContact = true;
+      },
+
+      showNewCard(colId) {
+        $('.column_master[data-id='+colId+'] .new-card').show();
+      },
+
+      hideNewCard(colId) {
+        $('.column_master[data-id='+colId+'] .new-card').hide();
+      },
+
+      addContact(userId, companyId, colId, data) {
+        var boardId = this.board_id;
+        var jwtToken = document.querySelector('meta[name="guardian_token"]').content;
+        if(data.name !== ''){
+          $.ajax('/api/v2/contact', {
+            method: 'POST',
+            data: {
+              'contact[user_id]': userId,
+              'contact[company_id]': companyId,
+              'contact[name]': data.name,
+              'contact[email]': data.email,
+              'contact[phone]': data.phone
+            },
+            headers: {'Authorization': 'Bearer '+jwtToken},
+            success: function(result){
+              var contactId = result.data.id;
+              $.ajax( '/api/v2/opportunity/' , {
+                method: 'POST',
+                headers: {'Authorization': 'Bearer '+jwtToken},
+                data: {
+                  'opportunity[main_contact_id]': contactId ,
+                  'opportunity[contact_ids]': [contactId],
+                  'opportunity[user_id]': userId,
+                  'opportunity[company_id]': companyId,
+                  'opportunity[board_id]': boardId,
+                  'opportunity[board_column_id]': colId,
+                  'opportunity[name]': ''
+                },
+                complete: function(xhr, status){
+                  window.location.reload();
+                }
+              });
+            }
+          });
+        }else{
+          alert('Name can\'t be blank');
+        }
       }
     },
     mounted() {
