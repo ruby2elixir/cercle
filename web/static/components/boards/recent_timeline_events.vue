6ff568bd142ac1559ef8990af96a18d7ce2a8705
Maxim Pechnikov <parallel588@gmail.com>
2017-04-24 20:19:17 +0300

    add user name, date of event to recent activity

--- web/static/components/boards/recent_timeline_events.vue
+++ web/static/components/boards/recent_timeline_events.vue
@@ -7,10 +7,11 @@
           <img :src="item.profile_image_url" style="max-width:40px;border-radius:40px;float:left;" />
           <div class="menu-info" style="margin-left:55px;">
             <h4 class="control-sidebar-subheading" style="font-size:16px;">
-              <span style="font-weight:600;">{{item.event_name}}</span>
+              <span style="font-weight:600;">{{item.user_name}}</span>
               <div>
                 {{item.content}}
               </div>
+              <small>{{item.created_at | moment('MMM DD [at] h:m A') }}</small>
             </h4>
           </div>
         </a>
@@ -20,8 +21,8 @@
   </div>
 </template>
 <script>
-    import {Socket, Presence} from 'phoenix';
-
+import {Socket, Presence} from 'phoenix';
+import moment from 'moment';
 export default {
       props: ['board_id'],
       data() {
