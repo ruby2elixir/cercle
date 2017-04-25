14fd56bc2b880b43851968e7b2ca3fdcec5a7b8e
Antoine <antoine@cercle.co>
2017-04-25 13:07:34 +0200

    add routes for user  & board column

--- web/controllers/api/v2/user_controller.ex
+++ web/controllers/api/v2/user_controller.ex
@@ -8,6 +8,19 @@ defmodule CercleApi.APIV2.UserController do
 
   plug :scrub_params, "user" when action in [:create, :update]
 
+  def index(conn, _params) do
+    current_user = Guardian.Plug.current_resource(conn)
+    company_id  = current_user.company_id
+    query = from u in User,
+      where: u.company_id == ^company_id,
+      order_by: [desc: u.updated_at]
+
+    users = query
+    |> Repo.all
+
+    render(conn, "index.json", users: users)
+  end
+
   def organizations(conn, _params) do
     current_user = Guardian.Plug.current_resource(conn)
     company_id = current_user.company_id
