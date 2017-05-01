612d176968e35f6176f6d361da52467f90bca90f
Harish Premkumar <p.harish86@gmail.com>
2017-05-01 18:47:01 +0000

    Updated tests to ensure timeline events and activites are deleted after the contact is deleted

--- test/controllers/api/v2/contact_controller_test.exs
+++ test/controllers/api/v2/contact_controller_test.exs
@@ -4,7 +4,7 @@ defmodule CercleApi.APIV2.ContactControllerTest do
 
   @valid_attrs %{name: "John Doe"}
   @invalid_attrs %{}
-  alias CercleApi.{Contact, TimelineEvent, Opportunity}
+  alias CercleApi.{Contact, TimelineEvent, Opportunity, Activity}
 
   setup %{conn: conn} do
     user = insert(:user)
@@ -131,6 +131,54 @@ defmodule CercleApi.APIV2.ContactControllerTest do
     opportunity = Repo.get(Opportunity, opportunity.id)
     assert opportunity.main_contact_id == contact2.id
     assert opportunity.contact_ids == [contact2.id]
+  end
+
+  test "deleting contact should delete associated timeline events", state do
+    changeset = Contact.changeset(%Contact{}, %{name: "Contact1", user_id: state[:user].id, company_id: state[:company].id})
+    contact = Repo.insert!(changeset)
+
+    changeset = Opportunity.changeset(%Opportunity{}, %{main_contact_id: contact.id, contact_ids: [contact.id], user_id: state[:user].id, company_id: state[:company].id})
+    opportunity = Repo.insert!(changeset)
+
+    changeset = TimelineEvent.changeset(%TimelineEvent{}, %{opportunity_id: opportunity.id, contact_id: contact.id, user_id: state[:user].id, company_id: state[:company].id, event_name: "test", content: "test"})
+    timeline_event = Repo.insert!(changeset)
+
+    old_count = Repo.one(from p in TimelineEvent, select: count("*"))
+
+    conn = delete state[:conn], "/api/v2/contact/#{contact.id}"
+    assert json_response(conn, 200)
+
+    # Test no of timeline events after delete
+    new_count = Repo.one(from p in TimelineEvent, select: count("*"))
+    assert old_count-1 == new_count
+
+    # timeline_event should be nil after deleting contact
+    timeline_event = Repo.get(TimelineEvent, timeline_event.id)
+    assert timeline_event == nil
+  end
+
+  test "deleting contact should delete associated activities", state do
+    changeset = Contact.changeset(%Contact{}, %{name: "Contact1", user_id: state[:user].id, company_id: state[:company].id})
+    contact = Repo.insert!(changeset)
+
+    changeset = Opportunity.changeset(%Opportunity{}, %{main_contact_id: contact.id, contact_ids: [contact.id], user_id: state[:user].id, company_id: state[:company].id})
+    opportunity = Repo.insert!(changeset)
+
+    changeset = Activity.changeset(%Activity{}, %{opportunity_id: opportunity.id, contact_id: contact.id, user_id: state[:user].id, company_id: state[:company].id, title: "test"})
+    activity = Repo.insert!(changeset)
+
+    old_count = Repo.one(from p in Activity, select: count("*"))
+
+    conn = delete state[:conn], "/api/v2/contact/#{contact.id}"
+    assert json_response(conn, 200)
+
+    # Test no of activities after delete
+    new_count = Repo.one(from p in Activity, select: count("*"))
+    assert old_count-1 == new_count
+
+    # Ensure activity is deleted
+    activity = Repo.get(Activity, activity.id)
+    assert activity == nil
   end
 
 end
