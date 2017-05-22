91286533acd87f0ed91c9b247a9e36fe20ce760c
Harish Premkumar <p.harish86@gmail.com>
2017-05-22 17:26:33 +0000

    Bug fix

--- priv/repo/update_contacts_name.exs
+++ priv/repo/update_contacts_name.exs
@@ -6,9 +6,11 @@ query = from p in Contact,
 contacts = Repo.all(query)
 
 Enum.each contacts, fn (contact) ->
-  splits = String.split(contact.first_name, ~r/\s+/)
-  [first_name|splits] = splits
-  last_name = Enum.join(splits, " ")
-  changeset = Contact.changeset(contact, %{:first_name => first_name, :last_name => last_name })
-  Repo.update(changeset)
+  if contact.first_name do
+    splits = String.split(contact.first_name, ~r/\s+/)
+    [first_name|splits] = splits
+    last_name = Enum.join(splits, " ")
+    changeset = Contact.changeset(contact, %{:first_name => first_name, :last_name => last_name })
+    Repo.update(changeset)
+  end
 end
