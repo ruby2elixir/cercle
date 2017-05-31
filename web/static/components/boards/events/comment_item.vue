3d59cd780a19d14519b06b2c49c33e7db29d561d
Maxim Pechnikov <parallel588@gmail.com>
2017-05-31 10:11:01 +0300

    add modal window for recent events

--- web/static/components/boards/events/comment_item.vue
+++ web/static/components/boards/events/comment_item.vue
@@ -1,51 +1,17 @@
 <template>
-  <li>
-    <a href="#">
-      <img :src="item.profile_image_url" style="max-width:40px;border-radius:40px;float:left;" />
-      <div class="menu-info" style="margin-left:55px;">
-       <h4 class="control-sidebar-subheading" style="font-size:16px;">
-         <span style="font-weight:600;">
-           {{item.user_name}}
-         </span>
-         about {{item.main_contact_name }}
-         <br />
-         <div class="comment-message" >
-           {{ item.content }}
-         </div>
-         <br />
-         <small>{{timestamp}}</small>
-       </h4>
-     </div>
-    </a>
-   </li>
+  <event :item="item" v-on:clickByEvent="$emit('clickByEvent')">
+    about {{item.main_contact_name }}
+    <br />
+    <div class="comment-message" >
+     {{ item.content }}
+    </div>
+  </event>
 </template>
 <script>
 import moment from 'moment';
+import EventTemplate from './base_template.vue';
 export default {
   props: ['item'],
-  methods: { },
-  computed: {
-    timestamp: function() {
-      return Moment.utc(this.item.created_at).fromNow();
-    }
-  },
-  mounted() { }
+  components: { 'event': EventTemplate }
 };
-  </script>
-<style lang="sass">
-  .comment-message {
-  padding: 9px 11px;
-  background-color: #fff;
-  border-radius: 3px;
-  box-shadow: 0 1px 2px rgba(0,0,0,.23);
-  -moz-box-sizing: border-box;
-  box-sizing: border-box;
-  clear: both;
-  cursor: pointer;
-  display: inline-block;
-  margin: 6px 2px 6px 0;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  max-width: 100%;
-  }
-</style>
+</script>
--- web/static/components/boards/recent_timeline_events.vue
+++ web/static/components/boards/recent_timeline_events.vue
@@ -2,8 +2,17 @@
   <div class="tab-pane active" id="control-sidebar-home-tab" >
     <h3 class="control-sidebar-heading">Recent Activity</h3>
     <ul class="control-sidebar-menu">
-      <component v-bind:is="item.event_name" v-for="item in items" :item="item" />
+      <component v-bind:is="item.event_name" v-for="item in items" :item="item" v-on:clickByEvent="clickByEvent(item)" />
     </ul>
+    <modal :show="openEventModal" style="margin-top: 74px;">
+      <div slot="modal-header" class="modal-header">
+      </div>
+      <div slot="modal-body" class="modal-body">
+        <ul><component v-bind:is="selectItem.event_name" :item="selectItem" /></ul>
+      </div>
+      <div slot="modal-footer" class="modal-footer">
+      </div>
+    </modal>
     <!-- /.control-sidebar-menu -->
   </div>
 </template>
@@ -19,26 +28,33 @@ export default {
   data() {
     return {
       channel: null,
-      items: [ ]
+      items: [ ],
+      openEventModal: false,
+      selectItem: {}
     };
   },
   components: {
-      'comment': CommentMessage,
-      'card.created': CardCreatedMessage,
-      'card.updated': CardUpdatedMessage
+    'comment': CommentMessage,
+    'card.created': CardCreatedMessage,
+    'card.updated': CardUpdatedMessage,
+    'modal': VueStrap.modal
   },
-    methods: {
-        eventAddOrUpdate(event) {
-            let itemIndex = this.$data.items.findIndex(function(item){
-                return item.id === parseInt(event.id);
-            });
-            if (itemIndex === -1){
-                this.items.unshift(event);
-            } else {
-                this.$data.items.splice(itemIndex, 1, event);
-            }
-        },
-      initConn() {
+  methods: {
+    clickByEvent(event) {
+      this.selectItem = event;
+      this.openEventModal = true;
+    },
+    eventAddOrUpdate(event) {
+      let itemIndex = this.$data.items.findIndex(function(item){
+        return item.id === parseInt(event.id);
+      });
+      if (itemIndex === -1){
+        this.items.unshift(event);
+      } else {
+        this.$data.items.splice(itemIndex, 1, event);
+      }
+    },
+    initConn() {
       this.socket = new Socket('/socket', {params: { token: localStorage.getItem('auth_token') }});
       this.socket.connect();
       this.channel = this.socket.channel('board:' + this.board_id, {});
@@ -51,10 +67,10 @@ export default {
       });
 
       this.channel.on('timeline_event:created', payload => {
-          this.eventAddOrUpdate(payload);
+        this.eventAddOrUpdate(payload);
       });
       this.channel.on('timeline_event:updated', payload => {
-         this.eventAddOrUpdate(payload);
+        this.eventAddOrUpdate(payload);
       });
       this.channel.on('timeline_event:deleted', payload => {
         let itemIndex = this.$data.items.findIndex(function(item){
