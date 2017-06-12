f7f652e75b831aee61e16723a08affafcd21bdbc
Harish Premkumar <p.harish86@gmail.com>
2017-06-12 19:54:10 +0000

    Updated board/column selection component

---
+++ web/static/components/shared/board-column-select-modal.vue
@@ -0,0 +1,108 @@
+<template>
+  <span v-on-click-outside='cancel'>
+    <span v-on:click="showModal" class='current-value-display'>
+      In {{ currentBoard().name }} - {{ currentColumn().name }}
+      <i class="fa fa-caret-down" />
+    </span>
+
+    <div v-show="editMode" class='input-modal'>
+      <div class='modal-header clearfix'>
+        Change Board/Column
+        <a class='close pull-right' v-on:click='cancel'>×</a>
+      </div>
+
+      <div class='modal-body'>
+        <div class="form-group">
+          <label>Board</label>
+          <select v-model="bId" @change='boardChange'>
+            <option v-for="board in boards" :value.number="board.id">{{board.name}}</option>
+          </select>
+        </div>
+        <div class="form-group">
+          <label>Column</label>
+          <select v-model="cId" ref="column-input">
+            <option v-for="column in selectedBoard().board_columns" :value.number="column.id">{{column.name}}</option>
+          </select>
+        </div>
+        <div>
+          <button class='btn btn-primary btn-block' @click='save'>Save</button>
+        </div>
+      </div>
+    </div>
+  </span>
+</template>
+
+<script>
+export default {
+  props: ['boardId', 'boardColumnId'],
+  data() {
+    return {
+      editMode: false,
+      bId: this.boardId,
+      cId: this.boardColumnId,
+      boards: [],
+      boardColumns: []
+    };
+  },
+  watch: {
+    'boardId': function() {
+      this.bId = this.boardId;
+    },
+    'boardColumnId': function() {
+      this.cId = this.boardColumnId;
+    }
+  },
+  methods: {
+    boardById(id) {
+      for(var i=0; i<this.boards.length; i++) {
+        if(id == this.boards[i].id)
+          return this.boards[i];
+      }
+    },
+    currentBoard() {
+      return this.boardById(this.boardId) || {name: '--', board_columns: []};
+    },
+    currentColumn() {
+      let cb = this.currentBoard();
+      for(var i=0; i<cb.board_columns.length; i++) {
+        if(this.boardColumnId == cb.board_columns[i].id)
+          return cb.board_columns[i];
+      }
+
+      return {name: '--'};
+    },
+    selectedBoard() {
+      return this.boardById(this.bId) || {board_columns: []};
+    },
+    boardChange() {
+      this.cId = null;
+    },
+    showModal() {
+      this.editMode = true;
+    },
+    save() {
+      this.$emit('input', {boardId: this.bId, boardColumnId: this.cId});
+      this.editMode = false;
+    },
+    cancel() {
+      this.bId = this.boardId;
+      this.cId = this.boardColumnId;
+      this.editMode = false;
+    }
+  },
+  mounted() {
+    this.$http.get('/api/v2/board/').then(resp => {
+      this.boards = resp.data.data;
+    });
+  }
+};
+</script>
+
+<style scoped>
+  .current-value-display {
+    color:rgb(99,99,99);
+    font-weight:bold;
+    font-size:24px;
+    cursor: pointer;
+  }
+</style>
