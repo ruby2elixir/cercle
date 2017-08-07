0317183ec2f23427df5e99899f78e8724a8a03d4
Maxim Pechnikov <parallel588@gmail.com>
2017-08-07 15:47:56 +0300

    add email templates

--- lib/cercleApi/tasks/activity_notification.ex
+++ lib/cercleApi/tasks/activity_notification.ex
@@ -1,73 +1,96 @@
 defmodule CercleApi.Tasks.ActivityNotification do
+
   require Logger
   import Ecto.Query, only: [from: 1, from: 2]
   use Timex
 
-  alias CercleApi.{Repo, Activity, User}
+  alias CercleApi.{Repo, Activity, Card, Notification, Mailer}
 
-  def due_date_start do
-    Logger.debug "start CercleApi.Tasks.ActivityNotification#due_date_start"
+  def run do
+    # {:ok, _} = Application.ensure_started(:timex)
+    #Application.ensure_all_started(:timex)
+    # Application.load(:tzdata)
+    # :ok = Application.ensure_started(:tzdata)
+    IO.puts "run ActivityNotification"
+    send_current_notification
+    send_prestart_notification
+  end
 
-    from_time = Timex.now |> Timex.shift(minutes: -1)
+  def send_current_notification do
+    from_time = Timex.now |> Timex.shift(minutes: -5)
     to_time = Timex.now |> Timex.shift(minutes: 1)
-
-    get_activities(from_time, to_time)
-    |> add_to_notifications
+    Enum.each(
+      get_items(from_time, to_time),
+      fn (item) -> send_email(item, "start") end
+    )
   end
 
-  def before_due_date do
-    from_time = Timex.now |> Timex.shift(hours: -1)
-    to_time = Timex.now
-    get_activities(from_time, to_time)
-    |> add_to_notifications
+  def send_prestart_notification do
+    from_time = Timex.now|> Timex.shift(minutes: 60)
+    to_time = Timex.now |> Timex.shift(minutes: 65)
+    Enum.each(
+      get_items(from_time, to_time),
+      fn (item) -> send_email(item, "before_start") end
+    )
   end
 
-  defp get_activities(from_time, to_time) do
-    query = from p in Activity,
-      where: p.due_date  >= ^from_time,
-      where: p.due_date <= ^to_time,
-      order_by: [asc: p.due_date]
-
-    query |> Repo.all
+  def send_email(item, "start") do
+    target = load_item(item)
+    if target.user.notification do
+      mail = %Mailman.Email{
+        subject: "Start",
+        from: "no-reply@cercle.co",
+        to: [target.user.login],
+        html: email_html(target, item.target_type)
+      }
+      Mailer.deliver(mail)
+      item
+      |> Notification.changeset(%{sent: true})
+      |> Repo.update
+    end
   end
 
-  def add_to_notifications(activities) do
-    activities
-    |> Enum.each(
-    fn (item) ->
-      with notification <- CercleApi.Notification.find_by_target("activity", item.id),
-      false <- is_nil(notification) do
-        update_notification(notification, item)
-      else
-        _ ->
-          create_notification(item)
-      end
-    end)
+  def send_email(item, "before_start") do
+    target = load_item(item)
+    if target.user.notification do
+      mail = %Mailman.Email{
+        subject: "Before Start",
+        from: "no-reply@cercle.co",
+        to: [target.user.login],
+        html: email_html(target, item.target_type)
+      }
+      Mailer.deliver(mail)
+    end
   end
 
-  def create_notification(activity) do
-    changeset = %CercleApi.Notification{}
-    |> CercleApi.Notification.changeset(
-      %{
-        target_type: "activity",
-        target_id: activity.id,
-        delivery_time: activity.due_date,
-        notification_type: "activity"
-      })
-
-    CercleApi.Repo.insert(changeset)
+  defp email_html(target, template_type) do
+    Phoenix.View.render_to_string(
+      CercleApi.EmailView, "start_#{template_type}.html",
+      target: target,
+      receiver: target.user.user_name
+    )
   end
 
-  def update_notification(notification, activity) do
-    changeset = notification
-    |> CercleApi.Notification.changeset(
-      %{
-        target_type: "activity",
-        target_id: activity.id,
-        delivery_time: activity.due_date,
-        notification_type: "activity"
-      })
-    CercleApi.Repo.update(changeset)
+  defp load_item(%{target_type: "card"} = item) do
+    Card
+    |> Repo.get_by(id: item.target_id)
+    |> Repo.preload([:user])
   end
 
+  defp load_item(%{target_type: "activity"} = item) do
+    Activity
+    |> Repo.get_by(id: item.target_id)
+    |> Repo.preload([:user])
+  end
+
+  defp get_items(from_time, to_time) do
+    query = from p in Notification,
+      where: p.delivery_time  >= ^from_time,
+      where: p.delivery_time <= ^to_time,
+      where: p.sent == false,
+      where: p.target_type in ["card", "active"],
+      order_by: [asc: p.delivery_time]
+
+    Repo.all(query)
+  end
 end
