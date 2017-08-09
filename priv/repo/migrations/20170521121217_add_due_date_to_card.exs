3aae8478d5020d084748ce18255b150f24a010fe
Antoine <antoine@cercle.co>
2017-08-09 10:23:47 +0200

    fix

--- priv/repo/migrations/20170521121217_add_due_date_to_card.exs
+++ priv/repo/migrations/20170521121217_add_due_date_to_card.exs
@@ -2,8 +2,8 @@ defmodule CercleApi.Repo.Migrations.AddDueDateToCard do
   use Ecto.Migration
 
   def change do
-    #alter table(:cards) do
-    #  add :due_date, :utc_datetime
-    #end
+    alter table(:cards) do
+      add :due_date, :utc_datetime
+    end
   end
 end
