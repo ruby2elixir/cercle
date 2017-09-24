38da70bfa09c294f60615ff04b09f28ab9665dca
Antoine <antoine@cercle.co>
2017-09-24 18:27:29 +0200

    fix error during migration

--- priv/repo/migrations/20170522050712_remove_main_contact_id_from_card.exs
+++ priv/repo/migrations/20170522050712_remove_main_contact_id_from_card.exs
@@ -2,18 +2,6 @@ defmodule CercleApi.Repo.Migrations.RemoveMainContactIdFromCard do
   use Ecto.Migration
 
   def up do
-
-    Enum.each(CercleApi.Repo.all(CercleApi.Card), fn (card)->
-      {:ok, res} = Ecto.Adapters.SQL.query(CercleApi.Repo,"select main_contact_id from cards where id = #{card.id}",[])
-      main_contact_id = List.first(List.first(res.rows))
-      if main_contact_id do
-        card_params = %{ main_contact_id: main_contact_id }
-        changeset = CercleApi.Card.changeset(card, card_params)
-        CercleApi.Repo.update(changeset)
-      end
-      card
-    end)
-
     alter table(:cards) do
       remove :main_contact_id
     end
