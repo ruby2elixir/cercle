7148ed21c992f3be2718981516d454cc703f16de
Maksim Pechnikov <parallel588@gmail.com>
2017-10-02 13:13:30 +0300

    add test

---
+++ test/features/add_contact_to_card_test.exs
@@ -0,0 +1,33 @@
+defmodule CercleApi.AddContactToCardCardTest do
+  use CercleApi.FeatureCase
+
+  test "Add contact to card", %{session: session} do
+    {user, company} = create_user_with_company()
+    board = CercleApi.Factory.insert(:board, company: company)
+    board_column = CercleApi.Factory.insert(:board_column, board: board)
+    CercleApi.Factory.insert(:card,
+      name: "Test Card",
+      user: nil,
+      company: company, board: board, board_column: board_column
+    )
+    CercleApi.Factory.insert(:contact,
+      first_name: "TestContact", last_name: "1",
+      company: company, user: user)
+
+    session
+    |> sign_in(user.login, "1234")
+    |> visit("/company/#{company.id}/board/#{board.id}")
+    |> click(css("span.name", text: "Test Card"))
+    |> assert_has(css("body.async-ready"))
+    |> click(button("ADD CONTACT"))
+    |> assert_has(css(".add-contact"))
+    |> fill_in(css(".add-contact input[type='search']"), with: "TestContact" )
+    |> click(css(".dropdown-menu a", text: "TestContact 1" ))
+    |> click(button("Add Contact"))
+    |> assert_has(css("body.async-ready"))
+    |> visit("/company/#{company.id}/board/#{board.id}")
+    |> click(css("span.name", text: "Test Card"))
+    |> assert_has(css("body.async-ready"))
+    |> assert_has(css(".card-contacts-list", text: "TestContact 1"))
+  end
+end
---
+++ test/features/add_member_to_card_test.exs
@@ -0,0 +1,31 @@
+defmodule CercleApi.AddMemberToCardCardTest do
+  use CercleApi.FeatureCase
+
+  test "Add/remove member to card", %{session: session} do
+    {user, company} = create_user_with_company()
+    board = CercleApi.Factory.insert(:board, company: company)
+    board_column = CercleApi.Factory.insert(:board_column, board: board)
+    CercleApi.Factory.insert(:card,
+      name: "Test Card",
+      user: nil,
+      company: company, board: board, board_column: board_column
+    )
+
+    session
+    |> sign_in(user.login, "1234")
+    |> visit("/company/#{company.id}/board/#{board.id}")
+    |> click(css("span.name", text: "Test Card"))
+    |> assert_has(css("body.async-ready"))
+    |> refute_has(css(".manager", text: user.user_name))
+    |> click(css(".card-select-member > span > span", text: "ADD MEMBER"))
+    |> click(css(".card-select-member ul.users-list > li"))
+    |> assert_has(css("body.async-ready"))
+    |> visit("/company/#{company.id}/board/#{board.id}")
+    |> click(css("span.name", text: "Test Card"))
+    |> assert_has(css("body.async-ready"))
+    |> assert_has(css(".manager", text: user.user_name))
+    |> click(css(".card-select-member > span > span", text: "CHANGE MEMBER"))
+    |> click(css(".card-select-member ul.users-list > li"))
+    |> refute_has(css(".manager", text: user.user_name))
+  end
+end
