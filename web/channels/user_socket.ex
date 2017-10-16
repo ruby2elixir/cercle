ede569e97fd67f1ec10bd03910dd6fa82ea7ff03
Antoine <antoine@cercle.co>
2017-10-16 22:38:00 +0200

    don't check origin of websocket

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -14,7 +14,8 @@ defmodule CercleApi.UserSocket do
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket,
-    timeout: 45_000
+    timeout: 45_000,
+    check_origin: false
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
