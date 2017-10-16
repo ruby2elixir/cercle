36a4281b0de2ab17e2abd72562c5587429f5884f
Antoine <antoine@cercle.co>
2017-10-16 23:19:52 +0200

    fix force ssl

--- config/prod.exs
+++ config/prod.exs
@@ -12,10 +12,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :cercleApi, CercleApi.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: System.get_env("HOST"), port: 80],
-  #url: [scheme: "https", host: "calm-river-78332.herokuapp.com", port: 443],
-  #force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  force_ssl: [hsts: true],
+  #url: [host: System.get_env("HOST"), port: 80],
+  url: [scheme: "https", host: System.get_env("HOST"), port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
