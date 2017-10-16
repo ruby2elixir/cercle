bb17b49a33d299f4dc2130497ddd944dddc219df
Antoine <antoine@cercle.co>
2017-10-16 23:16:25 +0200

    force ssl

--- config/prod.exs
+++ config/prod.exs
@@ -15,6 +15,7 @@ config :cercleApi, CercleApi.Endpoint,
   url: [host: System.get_env("HOST"), port: 80],
   #url: [scheme: "https", host: "calm-river-78332.herokuapp.com", port: 443],
   #force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  force_ssl: [hsts: true],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
