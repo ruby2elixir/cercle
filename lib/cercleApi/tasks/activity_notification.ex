9e0816af04152af134d76ff74a0159549e4b5598
Maksim Pechnikov <parallel588@gmail.com>
2017-11-15 08:00:52 +0300

    fix email notification

--- lib/cercleApi/tasks/activity_notification.ex
+++ lib/cercleApi/tasks/activity_notification.ex
@@ -17,8 +17,8 @@ defmodule CercleApi.Tasks.ActivityNotification do
 
   def send_email(%CercleApi.Notification{notification_type: "start"} = item) do
     with target <- load_item(item),
-         false <- is_nil(target), false <- is_nil(target.user),
-           true <- target.user.notification  do
+         false <- is_nil(target), false <- is_nil(target.due_date),
+           false <- is_nil(target.user), true <- target.user.notification  do
       mail = %Mailman.Email{
         subject: "#{target_name(target)} is due now",
         from: "referral@cercle.co", to: [target.user.login],
@@ -34,8 +34,8 @@ defmodule CercleApi.Tasks.ActivityNotification do
 
   def send_email(%CercleApi.Notification{notification_type: "prestart"} = item) do
     with target <- load_item(item),
-         false <- is_nil(target), false <- is_nil(target.user),
-           true <- target.user.notification do
+         false <- is_nil(target), false <- is_nil(target.due_date),
+           false <- is_nil(target.user), true <- target.user.notification do
       mail = %Mailman.Email{
         subject: "#{target_name(target)} is due soon",
         from: "referral@cercle.co", to: [target.user.login],
